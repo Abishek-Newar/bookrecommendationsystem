73e09aeeef276ce98087191adb0cd9984db489bd
abishejk1872 <abisheknewar15@gmail.com>
2024-07-21 20:49:26 +0530

    changes

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const pickle = require('picklejs'); 
+
+const app = express()
+
+
+
+app.listen(3000)
