23e254b75bed3f6b5edfe34bc34fded242d42239
abishejk1872 <abisheknewar15@gmail.com>
2024-07-30 16:42:23 +0530

    changes

--- app.py
+++ app.py
@@ -23,6 +23,16 @@ def fetch_poster(suggestion):
     for book_id in suggestion:
         book_name.append(book_pivot.index[book_id])
 
+    for i in book_name[0]:
+        ids = nm.where(final_rating['title'] == i)[0][0]
+        ids_index.append(ids)
+
+    for idx in ids_index:
+        url = final_rating.iloc[idx]['img_url'] 
+        poster_url.append(url)
+
+    return poster_url       
+
 
 def recommend_book(book_name):
     book_list = []
@@ -36,4 +46,21 @@ def recommend_book(book_name):
     return book_list, poster_url        
 
 if st.button('Show Recommendation'):
-    recommendation_books,poster_url = recommend_books(selected_books)
+    recommendation_books,poster_url = recommend_book(selected_books)
+    col1,col2,col3,col4,col5 = st.columns(5)
+
+    with col1:
+        st.text(recommendation_books[1])
+        st.image(poster_url[1])
+    with col2:
+        st.text(recommendation_books[2])
+        st.image(poster_url[2])        
+    with col3:
+        st.text(recommendation_books[3])
+        st.image(poster_url[3])
+    with col4:
+        st.text(recommendation_books[4])
+        st.image(poster_url[4])
+    with col5:
+        st.text(recommendation_books[5])
+        st.image(poster_url[5])
